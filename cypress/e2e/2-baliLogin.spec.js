802d9df1ac358c9f3ee3bdc49cbaa08db0ebd18c
Elena Dyagileva <edyagileva96@gmail.com>
2023-02-22 14:55:08 +0700

    Place order test

--- cypress/e2e/2-baliLogin.spec.js
+++ cypress/e2e/2-baliLogin.spec.js
@@ -12,37 +12,58 @@ describe("Login", () => {
     cy.get("h1").should("contain", "Your account");
   });
 
-  function totalPrice() {
-    const productPrice = document
-      .getElementsByClassName("current-price")[0]
-      .innerText.slice(2);
-    const quantity = document
-      .getElementsByClassName("js-cart-line-product-quantity form-control")
-      .item(0).value;
-    const totalPriceCalculated = productPrice * quantity;
-    const totalPriceDisplayed = document
-      .getElementsByClassName("price-total")[1]
-      .innerHTML.slice(2);
-    return totalPriceCalculated, totalPriceDisplayed;
-  }
-
   it("verify nav", () => {
     cy.get(".anav-top").eq(0).find("li").should("have.length", 8);
   });
 
   it.only("order broccoli", () => {
     cy.contains("Veggies & Fruits").click();
-    cy.contains("Broccoli").click({ force: true });
-    cy.wait(5000);
     cy.contains("Add to cart").eq(0).click();
-    cy.contains("Veggies & Fruits").click();
-    //cy.get(".an_wishlist-icon").eq(0).click();
     cy.contains("Shopping Cart").click();
-
-    cy.contains("Proceed to checkout").click({ force: true });
+    cy.get(".cart-action a").eq(1).click({ force: true });
     cy.url().should("contain", "/cart?action=show");
-    cy.contains("+").click();
 
-    cy.get(".product-price").should("eq", totalPrice.totalPriceCalculated);
+    cy.get(".current-price")
+      .invoke("text")
+      .then((text) => {
+        const priceString = text.replace(/[^\d.-]/g, "");
+        const price = parseFloat(priceString);
+        cy.wrap(price).as("current-price");
+      });
+
+    cy.get(".cart-price-row input")
+      .invoke("val")
+      .then((value) => {
+        const quantity = Number(value);
+        cy.wrap(quantity).as("quantity");
+      });
+
+    cy.get("@current-price").then((currentPrice) => {
+      cy.get("@quantity").then((quantity) => {
+        const totalPriceCalculated = currentPrice * quantity;
+
+        cy.get(".cart-price-row .product-price")
+          .invoke("text")
+          .then((text) => {
+            const priceString = text.replace(/[^\d.-]/g, "");
+            const price = parseFloat(priceString);
+            cy.wrap(price);
+          })
+          .should("eq", totalPriceCalculated);
+      });
+    });
+
+    cy.contains("Checkout").click();
+    /*cy.get('[name="address1"]').type("Mejan");
+    cy.get('[name="city"]').type("Canggu");
+    cy.get("select").select("385").should("contain", "Canggu to Pererenan");
+    cy.get('[name="phone_mobile"]').type("+6282147372752");
+    */
+    cy.get(".addresses-continue > .btn").click();
+    cy.get("#js-delivery > .continue").click();
+    cy.get("#payment-option-1").check().should("be.checked");
+    cy.get("#conditions_to_approve").click();
+    // cy.get('.ps-shown-by-js > .btn').click().should(); // place order
+    //cy.url().should('include', '/order-confirmation');
   });
 });
