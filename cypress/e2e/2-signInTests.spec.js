bec54688523364c67599bc653a1830838ceaeb14
Elena Dyagileva <edyagileva96@gmail.com>
2023-03-05 13:09:31 +0800

    refactored sign in tests

--- cypress/e2e/2-signInTests.spec.js
+++ cypress/e2e/2-signInTests.spec.js
@@ -4,38 +4,37 @@ describe("Sign in tests", () => {
     cy.get(".notification_cookie-accept").click();
   });
 
-  it("verify successful login", () => {
-    cy.get('[placeholder="Email"]').type("luzefegy@finews.biz");
-    cy.get('[placeholder="Password"]').type("itsmeHello1!");
+  function enterDate(name, password) {
+    cy.get('[placeholder="Email"]').type(name);
+    cy.get('[placeholder="Password"]').type(password);
     cy.get("#submit-login").click();
+  }
+
+  it("verify successful login", () => {
+    enterDate("luzefegy@finews.biz", "itsmeHello1!");
     cy.url().should("contain", "my-account");
   });
 
   it("verify unsuccessful login with wrong password and registered email", () => {
-    cy.get('[placeholder="Email"]').type("luzefegy@finews.biz");
-    cy.get('[placeholder="Password"]').type("itsmeHello1");
-    cy.get("#submit-login").click();
+    enterDate("luzefegy@finews.biz", "itsmeHello");
     cy.get(".help-block").should("contain", "Authentication failed."); // not good UI (user doesn't know why it failed)
   });
 
   it("verify unsuccessful login with unregistered email", () => {
-    cy.get('[placeholder="Email"]').type("luzef@finews.biz");
-    cy.get('[placeholder="Password"]').type("itsmeHello1");
-    cy.get("#submit-login").click();
+    enterDate("luz@finews.biz", "itsmeHello1!");
     cy.get(".help-block").should("contain", "Authentication failed."); // not good UI (user doesn't know why it failed)
   });
 
-  it("verify unsuccessful login with blank password", () => {
-    cy.get('[placeholder="Email"]').type("luzefegy@finews.biz");
+  it.only("verify unsuccessful login with blank password", () => {
+    cy.get('[placeholder="Email"]').type("luz@finews.biz");
     cy.get("#submit-login").click();
     cy.on("window:alert", (str) => {
       expect(str).to.equal(`Please fill in this field.`); // requires to click on agree
     });
   });
 
-  it("verify unsuccessful login with blank email", () => {
-    cy.get('[placeholder="Password"]').type("itsmeHello1!");
-    cy.get("#submit-login").click();
+  it.only("verify unsuccessful login with blank email", () => {
+    enterDate(" ", "itsmeHello1!");
     cy.on("window:alert", (str) => {
       expect(str).to.equal(`Please fill in this field.`); // requires to click on agree
     });
